2606abf4f5f0cf654ef568380f02db492839ca88
brooks <bpoltl1@gmail.com>
2019-02-10 06:26:50 -0600

    added axios

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -8,10 +8,7 @@ const {
     GraphQLSchema
 } = graphql;
 
-const users = [
-    {id: '23', firstName: 'Bill', age:20},
-    {id: '47', firstName: 'Samantha', age:21}
-]
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
